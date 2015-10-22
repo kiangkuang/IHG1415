decaa7a4f30a5ce6322907e02f0aeb724c404db0
Kiang Kuang <kiangkuang@gmail.com>
2015-10-23 01:04:39 +0800

    fix index.php

--- index.php
+++ index.php
@@ -1,98 +1,17 @@
+<?php
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
 
-<html><head><meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<title>Moroccan Hacker's</title>
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define('WP_USE_THEMES', true);
 
-
-<script src="http://im83.gulfup.com/lrb8yN.jpg" type="text/javascript">
-    </script>
-    <script type="text/javascript">$(document).ready(function() {$(".facebookbox").hover(function() {$(this).stop().animate({right: "0"}, "medium");}, function() {$(this).stop().animate({right: "-250"}, "medium");}, 500);});
-    </script><style type="text/css">
-    .facebookbox{background: url("http://www9.0zz0.com/2012/12/05/17/644420689.png")
-     no-repeat scroll left center transparent !important;display: block;float: right;height: 270px;padding: 0 5px 0 46px;width: 245px;z-index: 99999;position:fixed;right:-250px;top:40%;}
-    .facebookbox div{border:none;position:relative;display:block;}
-    .facebookbox span{bottom: 12px;font: 8px "lucida grande",tahoma,verdana,arial,sans-serif;position: absolute;right: 6px;text-align: right;z-index: 99999;}
-    .facebookbox span a{color: #808080;text-decoration:none;}
-    .facebookbox span a:hover{text-decoration:underline;}</style>
-<style type="text/css">
-
-body {
-	background-color: #060606;
-}
-.anass{
-	haight: 20px;
-	}
-.Style1 {
-	color: #FFFFFF;
-	font-family: "Courier New", Courier, monospace;
-}
-.Style3 {color: #99FF66}
-.Style4 {
-	color: #FF0000;
-	font-weight: bold;
-}
-.Style5 {
-	color: #66CCCC;
-	font-weight: bold;
-}
-.Style5:hover{
-	background: #000;
-	color: #D7241A;
-	}
-.Style6 {color: #66FFFF}
-.Style7 {
-	color: #00CCFF;
-	font-weight: bold;
-}
-.Style8 {
-	color: #FFFFFF;
-	
-	}
-.Style8 a{
-	text-decoration: none;
-	color: #FFFFFF;
-	}
-.Style8 a:hover{
-		background: #DF002F;
-		color: #000;
-		border: 1px dotted #C18206 ;
-	}
-.Style9 {color: #33FFFF}
-
-</style></head>
-
-
-<body>	
-<div class="Style1" align="center">
-  <h1 class="Style5">Hacked By Thex@b1</h1>
-
-  <p><img src="http://store2.up-00.com/2015-03/1427834784651.jpg" width="400" height="200"></p>
- 
-<p class="Style4" align="center"> ./Fuck Isreal And USE</p>
-  <p class="Style3" align="center">!! I'm Muslims I'm The Voice Of Palestinian People !!
-
-  <br><br><center>
-  
-  <embed src="https://youtube.googleapis.com/v/CgALu68DFgg&amp;feature=related&amp;autoplay=1&amp;loop=1" type="application/x-shockwave-flash" wmode="transparent" width="640" height="480">
-<br><br><font color="red" size="6"><b>#THE TRUTH WILL PREVAIL</b><br></font>
-</center>
-  
-  </br></br></center>
-
-</body></html>
-<p>
-</p><p id="anima" style="position:absolute;font-size:20;background-color:white;">
-</p>
-
-  <p></p><p></p>
-  
- <h3 class="Style8">
-	  <a href="">Fuck Governement Use</a>
-	  </h3>
-</div>
-<p></p><center><script type="text/javascript" language="javascript" src="./HaCkeD By vb_files/2.js"></script><div style="display:block;border:1px solid white;width:150px;height:45px;font-family:Verdana,Arial,Helvetica,sans-serif;font-size:11px;text-align:center;line-height:16px;cursor:pointer;background:black;color:white" onclick="location.href=&#39;http://en.dnstools.ch/show-my-ip.html&#39;;" onmouseover="self.status=&#39;What is my IP?&#39;; return true;" onmouseout="self.status=&#39;&#39;;return true;"><ul style="list-style-type:none;margin:3px;padding:0"><li>Your current IP:</li><li style="font-size:13px;font-weight:bold;margin:2px">41.137.21.103</li></ul></div><noscript>&lt;a href="http://en.dnstools.ch/show-my-ip.html" target="_blank"&gt;IP address signature&lt;/a&gt;</noscript></center>
-
-<embed src="" type="application/x-shockwave-flash" wmode="transparent" width="1" height="1">
-<div id="bar" style="position: fixed; width: 100%; bottom: 0px; font-family: tahoma; height: 20px; color: #ccc; font-size: 13px; left: 0px; border-top: 1px solid #222; padding: 5px; background-color: #222"> 
-<center><b>&nbsp;Copyright &#195;&#130;&#194;© Thex@b1</b></center></div>
-<br><br>
-</body></html>
+/** Loads the WordPress Environment and Template */
+require( dirname( __FILE__ ) . '/wp-blog-header.php' );
