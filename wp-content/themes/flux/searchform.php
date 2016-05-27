782bbc6cafc345f2d1cd9ae00bcee27565e64616
Kiang Kuang <kiangkuang@gmail.com>
2016-05-28 00:20:58 +0800

    static pages

--- wp-content/themes/flux/searchform.php
+++
@@ -1,5 +0,0 @@
-<form id="searchform" action="<?php echo home_url();?>" method="get" >
-		<fieldset class="search-fieldset">
-			<input type="text" id="s" name="s" value="<?php _e('Type and hit enter','indonez');?>" onblur="if (this.value == ''){this.value = '<?php _e('Type and hit enter','indonez');?>'; }" onfocus="if (this.value == '<?php _e('Type and hit enter','indonez');?>') {this.value = ''; }" />
-		</fieldset>      						
-</form>
